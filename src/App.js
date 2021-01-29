b4843ab6b70e998feab524070220b56e58517b5e
nachosbeans <nachosbeans@gmail.com>
2021-01-29 01:34:06 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isCancelled, setIsCancelled] = useState(false);
+
+  let bruh = new Audio('/bruh.mp3');
+
+  function getCancelled() {
+    bruh.play();
+    setIsCancelled(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(isCancelled) ? "App-cancelled" : "App"}>
+      <button onClick={getCancelled} className={(isCancelled) ? 'Cancelled-button' : 'Cancel-button'}>
+        {(isCancelled) ? 'CANCELLED, BITCH' : 'GET CANCELLED'}
+      </button>
     </div>
   );
 }
